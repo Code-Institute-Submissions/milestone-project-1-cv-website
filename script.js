ea405389540e8f398a9c8972a33e793e732235dd
kairosity <karinafinegan@gmail.com>
2020-08-09 19:33:42 +0000

    Tidy the jQuery code and make some notes at end of jQuery code

--- script.js
+++ script.js
@@ -5,41 +5,34 @@
 // On screens smaller than 768 - do this: 
 //BUT ALSO: Need to have this NOT apply to the last icon on the timeline - the sun. How????
 
-//The code below works EXCEPT when resizing the windows. Then it gets weird. - Need to fix that somehow?
 
 $(document).ready(function () {
 
      if ( $(window).width() < 768 ) {
 
             $(".timeline-icon").mouseover(function () {
-                console.log("window is small");
-                console.log($(this))
-                // $(".timeline-info").css("display", "none"); // hide all timeline-infos
-                $(this).parent().parent().find(".timeline-info").css("display", "block"); // show current timeline info
+    
+                $(this).parent().parent().find(".timeline-info").css("display", "block"); // show current ('this') timeline info
                 $(this).css("visibility", "hidden");
                 $(this).parent().parent().next().find(".timeline-line").css("visibility", "hidden"); //this would be better with z-index but can't seem to make that work? 
             
             })
             $(".timeline-icon").mouseout(function () {
-                $(".timeline-info").css("display", "none"); // hide all timeline infos
+                $(".timeline-info").css("display", "none"); // hide the timeline info
                 $(this).css("visibility", "visible");
-                $(this).parent().parent().next().find(".timeline-line").css("visibility", "visible"); //this would be better with z-index but can't seem to make that work? 
+                $(this).parent().parent().next().find(".timeline-line").css("visibility", "visible"); 
                 
             })
 
         } else if ( $(window).width() >= 768 ) {
+
         //On large screens do this: 
              $(".timeline-icon").mouseover(function () {
-
-                console.log("window is large");
-                console.log($(this))
-                // $(".timeline-info").css("display", "none"); // hide all timeline-info
                 $(this).parent().parent().find(".timeline-info").css("display", "block"); // show current timeline info
-           
                 })
 
                 $(".timeline-icon").mouseout(function () {
-                $(".timeline-info").css("display", "none"); // hide all timeline infos
+                $(".timeline-info").css("display", "none"); // hide the timeline info
             
                 })
 
@@ -48,34 +41,27 @@ $(document).ready(function () {
 
 $(window).resize(function () {
 
-    console.log( $(window).width() )
      if ( $(window).width() < 768 ) {
          console.log("window went under 768")
 
             $(".timeline-icon").mouseover(function () {
-                console.log("window is small");
-                console.log($(this))
-                // $(".timeline-info").css("display", "none"); // hide all timeline-infos
+                
                 $(this).parent().parent().find(".timeline-info").css("display", "block"); // show current timeline info
                 $(this).css("visibility", "hidden");
-                $(this).parent().parent().next().find(".timeline-line").css("visibility", "hidden"); //this would be better with z-index but can't seem to make that work? 
-            
+                $(this).parent().parent().next().find(".timeline-line").css("visibility", "hidden");  
             })
+
             $(".timeline-icon").mouseout(function () {
                 $(".timeline-info").css("display", "none"); // hide all timeline infos
                 $(this).css("visibility", "visible");
-                $(this).parent().parent().next().find(".timeline-line").css("visibility", "visible"); //this would be better with z-index but can't seem to make that work? 
-                
+                $(this).parent().parent().next().find(".timeline-line").css("visibility", "visible");              
             })
 
         } else if ( $(window).width() >= 768 ) {
             console.log("window went over 768")
-        //On large screens do this: 
-             $(".timeline-icon").mouseover(function () {
+            //On large screens do this: 
 
-                console.log("window is large");
-                console.log($(this))
-                // $(".timeline-info").css("display", "none"); // hide all timeline-info
+             $(".timeline-icon").mouseover(function () {
                 $(this).css("visibility", "visible");
                 $(this).parent().parent().find(".timeline-info").css("display", "block"); // show current timeline info
                 $(this).parent().parent().next().find(".timeline-line").css("visibility", "visible");
@@ -89,6 +75,8 @@ $(window).resize(function () {
             }
 });
 
+//There must be a shorter way to write the above. Something like "When the document is ready OR when the window is resized - do all this:"
 
+//Also - resizing to small is fine on mobile, but in small windows on mouse hover on desktop there is an annoying flickering effect. Don't know why. 
 
 
