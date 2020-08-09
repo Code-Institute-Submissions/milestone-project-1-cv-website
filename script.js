96297134a115141389cb6c0627247bea6ea004ad
kairosity <karinafinegan@gmail.com>
2020-08-09 17:36:35 +0000

    Add visibility rule to jQuery to hide icon when timeline info is displayed

--- script.js
+++ script.js
@@ -3,11 +3,15 @@
 $(document).ready(function () {
     $(".timeline-icon").mouseover(function () {
         console.log("working");
-        console.log($(this).parent().parent())
+        console.log($(this))
         // $(".timeline-info").css("display", "none"); // hide all timeline-infos
         $(this).parent().parent().find(".timeline-info").css("display", "block"); // show current timeline info
+        $(this).css("visibility", "hidden");
+       
     })
     $(".timeline-icon").mouseout(function () {
         $(".timeline-info").css("display", "none"); // hide all timeline infos
+        $(this).css("visibility", "visible");
+        
     })
 });
