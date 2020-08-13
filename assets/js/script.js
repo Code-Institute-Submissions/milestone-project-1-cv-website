a20fb909b33ce0e99d6ea3248c80249119e8bf77
kairosity <karinafinegan@gmail.com>
2020-08-13 08:37:54 +0000

    Refactor jQuery code

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,7 @@ $(window).resize(function () {
     3. Hide the icon that was clicked.
     4. Hide the timeline line following that clicked icon.
 
-    The following is only applied to small screens:
+    The following is only applied to small screens and is included as a separate function: addBlurDarken
    
     5. Blur and darken all the other timeline icons.
     6. Blur and darken all the other timeline lines.
@@ -31,9 +31,6 @@ $(window).resize(function () {
     10. Blur and darken the scrolling arrow and text. 
 
     Then all this is undone on mouseout. 
-
-    Lines 83-89 are to fix the edge-case scenario whereby a user looks at the timeline on a small screen on a desktop and then resizes it
-    to a large screen. The blur and darken effect was travelling, so these lines stop that unwanted behaviour.
 
 */
 
@@ -49,13 +46,7 @@ function toggleTimeline() {
                 $(this).css("visibility", "hidden"); //hide icon clicked
                 $(this).parent().parent().next().find(".timeline-line").css("visibility", "hidden"); //hide line following icon clicked
                 
-                workHistorySection.find(".timeline-icon").addClass("blur-and-darken"); 
-                workHistorySection.find(".timeline-line").addClass("blur-and-darken"); 
-                $(".work-history-heading").addClass("blur-and-darken");
-                $(".timeline-instruction-device").addClass("blur-and-darken");
-                $(".timeline-instruction-non-device").addClass("blur-and-darken");
-                $(".end-of-timeline").addClass("blur-and-darken");
-                $(".arrow-down-history-to-skills").addClass("blur-and-darken");
+                addBlurDarken()
             })
 
             $(".timeline-icon").mouseout(function () {
@@ -63,39 +54,47 @@ function toggleTimeline() {
                 $(this).css("visibility", "visible"); //show icon clicked
                 $(this).parent().parent().next().find(".timeline-line").css("visibility", "visible"); //show line following icon clicked
                 
-                workHistorySection.find(".timeline-icon").removeClass("blur-and-darken"); 
-                workHistorySection.find(".timeline-line").removeClass("blur-and-darken");
-                $(".work-history-heading").removeClass("blur-and-darken");
-                $(".timeline-instruction-device").removeClass("blur-and-darken");
-                $(".timeline-instruction-non-device").removeClass("blur-and-darken");
-                $(".end-of-timeline").removeClass("blur-and-darken");
-                $(".arrow-down-history-to-skills").removeClass("blur-and-darken");             
+                removeBlurDarken();            
             })
 
         } else if ( $(window).width() >= 768 ) {
 
 
-             $(".timeline-icon").mouseover(function () {
+            $(".timeline-icon").mouseover(function () {
                 $(this).css("visibility", "visible");
                 $(this).parent().parent().find(".timeline-info").css("display", "block"); // show current timeline info
                 $(this).parent().parent().next().find(".timeline-line").css("visibility", "visible");
 
-                workHistorySection.find(".timeline-icon").removeClass("blur-and-darken");
-                workHistorySection.find(".timeline-line").removeClass("blur-and-darken");
-                $(".work-history-heading").removeClass("blur-and-darken");
-                $(".timeline-instruction-device").removeClass("blur-and-darken");
-                $(".timeline-instruction-non-device").removeClass("blur-and-darken");
-                $(".end-of-timeline").removeClass("blur-and-darken");
-                $(".arrow-down-history-to-skills").removeClass("blur-and-darken");  
-                
+                removeBlurDarken()      
                 })
 
-                $(".timeline-icon").mouseout(function () {
+            $(".timeline-icon").mouseout(function () {
                 $(".timeline-info").css("display", "none"); // hide all timeline infos
             
-                })
+            })
 
-            }
+        }
 }
 
+// I was repeating a lot of this code so I separated it out into two functions: 
+
+function addBlurDarken() {
+    workHistorySection.find(".timeline-icon").addClass("blur-and-darken"); 
+    workHistorySection.find(".timeline-line").addClass("blur-and-darken"); 
+    $(".work-history-heading").addClass("blur-and-darken");
+    $(".timeline-instruction-device").addClass("blur-and-darken");
+    $(".timeline-instruction-non-device").addClass("blur-and-darken");
+    $(".end-of-timeline").addClass("blur-and-darken");
+    $(".arrow-down-history-to-skills").addClass("blur-and-darken");
+}
+
+function removeBlurDarken() {
+    workHistorySection.find(".timeline-icon").removeClass("blur-and-darken");
+    workHistorySection.find(".timeline-line").removeClass("blur-and-darken");
+    $(".work-history-heading").removeClass("blur-and-darken");
+    $(".timeline-instruction-device").removeClass("blur-and-darken");
+    $(".timeline-instruction-non-device").removeClass("blur-and-darken");
+    $(".end-of-timeline").removeClass("blur-and-darken");
+    $(".arrow-down-history-to-skills").removeClass("blur-and-darken");  
+}
 
