2bad3c09b7d3f106829e7cb4557232790d753fbe
kairosity <karinafinegan@gmail.com>
2020-08-13 07:44:34 +0000

    Add jquery code to toggle a blur & darker background when timeline info is up

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@
 // Also used the first line of @dynamyc-2's code from CSS Tricks: https://css-tricks.com/forums/topic/jquery-window-width-condition/
 
 
-// On screens smaller than 768 - do this: 
 //BUT ALSO: Need to have this NOT apply to the last icon on the timeline - the sun. How????
 
 
@@ -14,7 +13,28 @@ $(window).resize(function () {
     toggleTimeline()
 });
 
-/* Timeline toggle function */
+
+/* Timeline toggle function
+
+    1. This function starts by dividing the logic into two sections small screens and large screens.
+    2. Then on mouse over (click or hover): show the timeline information.
+    3. Hide the icon that was clicked.
+    4. Hide the timeline line following that clicked icon.
+
+    The following is only applied to small screens:
+   
+    5. Blur and darken all the other timeline icons.
+    6. Blur and darken all the other timeline lines.
+    7. Blur and darken the Work History Heading.
+    8. Blur and darken the instructions under the heading
+    9. Blur and darken the paragraph at the end of the timeline.
+    10. Blur and darken the scrolling arrow and text. 
+
+    Then all this is undone on mouseout. 
+
+*/
+
+const workHistorySection = $("#work-history-section");
 
 function toggleTimeline() {
     if ( $(window).width() < 768 ) {
@@ -22,15 +42,29 @@ function toggleTimeline() {
 
             $(".timeline-icon").mouseover(function () {
                 
-                $(this).parent().parent().find(".timeline-info").css("display", "block"); // show current timeline info
-                $(this).css("visibility", "hidden");
-                $(this).parent().parent().next().find(".timeline-line").css("visibility", "hidden");  
+                $(this).parent().parent().find(".timeline-info").css("display", "block"); //show timeline info
+                $(this).css("visibility", "hidden"); //hide icon clicked
+                $(this).parent().parent().next().find(".timeline-line").css("visibility", "hidden"); //hide line following icon clicked
+                
+                workHistorySection.find(".timeline-icon").addClass("blur-and-darken"); 
+                workHistorySection.find(".timeline-line").addClass("blur-and-darken"); 
+                $(".work-history-heading").addClass("blur-and-darken");
+                $(".timeline-instruction-device").addClass("blur-and-darken");
+                $(".end-of-timeline").addClass("blur-and-darken");
+                $(".arrow-down-history-to-skills").addClass("blur-and-darken");
             })
 
             $(".timeline-icon").mouseout(function () {
-                $(".timeline-info").css("display", "none"); // hide all timeline infos
-                $(this).css("visibility", "visible");
-                $(this).parent().parent().next().find(".timeline-line").css("visibility", "visible");              
+                $(".timeline-info").css("display", "none"); //hide timeline info
+                $(this).css("visibility", "visible"); //show icon clicked
+                $(this).parent().parent().next().find(".timeline-line").css("visibility", "visible"); //show line following icon clicked
+                
+                workHistorySection.find(".timeline-icon").removeClass("blur-and-darken"); 
+                workHistorySection.find(".timeline-line").removeClass("blur-and-darken");
+                $(".work-history-heading").removeClass("blur-and-darken");
+                $(".timeline-instruction-device").removeClass("blur-and-darken");
+                $(".end-of-timeline").removeClass("blur-and-darken");
+                $(".arrow-down-history-to-skills").removeClass("blur-and-darken");             
             })
 
         } else if ( $(window).width() >= 768 ) {
@@ -48,4 +82,3 @@ function toggleTimeline() {
 
             }
 }
-
